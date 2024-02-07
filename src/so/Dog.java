d2bb3e1770dda101be69f78efdbf890095e7e017
exam <exam@Bcalab4-m01>
2024-02-07 10:12:02 +0530

    Commit 1

---
+++ src/so/Dog.java
@@ -0,0 +1,21 @@
+package so;
+
+public class Dog {
+	
+	private String name;
+	private String breed;
+	
+	public Dog(String name,String breed) {
+		this.name = name;
+		this.breed = breed;
+	}
+	public String getBreed() {
+		return breed;
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
